bcc484bbba0b3b6898ed2c03bd10152550f58825
selva <selvamuthukumar.thiruarul@disney.com>
2017-07-27 13:29:16 -0400

    Modify main.py

--- process_twitter_visualize/main.py
+++ process_twitter_visualize/main.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
 import argparse
+import json
 from twython import Twython
-APP_KEY = 'xx'
-APP_SECRET = 'xx'
+CONSUMER_KEY = 'vG39KwmuObu56TmYpHk5q6niM'
+CONSUMER_SECRET = 'mGxwHb3iNnsGg9P3kJKvWctPL2UjHBRWo6i9ET6wUrEVArnbSR'
 
 def parse_args():
     """
@@ -13,25 +14,30 @@ def parse_args():
     parser = argparse.ArgumentParser(description='<usage> python main.py -k <KEYWORD>')
     parser.add_argument('--keyword', '-k', action='store', default='pyspark',
                         help='<usage> python main.py -k <KEYWORD>')
+    parser.add_argument('--no', '-n', action='store', default='5',
+                        help='<usage> python main.py -n <NUMBER_OF_TWEETS>')
     arguments, _ = parser.parse_known_args()
     return arguments
 
 
-def check():
+def check(keyword,no_of_tweets=0):
 
-    twitter = Twython(APP_KEY, APP_SECRET, oauth_version=2)
+    twitter = Twython(CONSUMER_KEY, CONSUMER_SECRET, oauth_version=2)
     ACCESS_TOKEN = twitter.obtain_access_token()
-    twitter = Twython(APP_KEY, access_token=ACCESS_TOKEN)
-    results = twitter.cursor(twitter.search, q='')
-    for result in results:
-        print result
-        break
-
+    twitter = Twython(CONSUMER_KEY, access_token=ACCESS_TOKEN)
+    tweets = []
+    for status in twitter.search(q='\"{}\"'.format(keyword))['statuses']:
+        if len(tweets)<no_of_tweets:
+            text = status['text'].encode('utf-8')
+            tweets.append(text)
+    with open('tweet_search_{}.json'.format(keyword), 'w') as f:
+        json.dump(tweets, f, indent=4)
 
 
 
 if __name__ == '__main__':
     args = parse_args()
     keyword = args.keyword
+    no_of_tweets=args.no
     print(keyword)
-    check()
+    check(keyword,no_of_tweets)
