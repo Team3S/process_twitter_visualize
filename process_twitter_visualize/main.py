7f0e5a89c3526b44a0066e1f563199acf6d5ef08
sathyendrasaran <sathyendrasaran@gmail.com>
2017-08-31 10:16:37 -0500

    Changest to Twitter code

--- process_twitter_visualize/main.py
+++ process_twitter_visualize/main.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
+"""
+Created on Thu Oct 20 18:31:10 2016
+
+@author: sathyendrasaran
+"""
+
 import argparse
 import json
 from twython import Twython
-import os
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""
 
 
 def parse_args():
@@ -14,9 +19,9 @@ def parse_args():
     :return:
     """
     parser = argparse.ArgumentParser(description='<usage> python main.py -k <KEYWORD>')
-    parser.add_argument('--keyword', '-k', action='store', default='pyspark',
+    parser.add_argument('--keyword', '-k', action='store', default='purch',
                         help='<usage> python main.py -k <KEYWORD>')
-    parser.add_argument('--no', '-n', action='store', default='5',
+    parser.add_argument('--no', '-n', action='store', default='100',
                         help='<usage> python main.py -n <NUMBER_OF_TWEETS>')
     arguments, _ = parser.parse_known_args()
     return arguments
@@ -29,8 +34,8 @@ def check(keyword,no_of_tweets=0):
     twitter = Twython(CONSUMER_KEY, access_token=ACCESS_TOKEN)
     tweets = []
     for status in twitter.search(q='\"{}\"'.format(keyword))['statuses']:
-        if len(tweets)<no_of_tweets:
-            text = status['text'].encode('utf-8')
+        if len(tweets) < no_of_tweets:
+            text = status['text']
             tweets.append(text)
     with open('tweet_search_{}.json'.format(keyword), 'w') as f:
         json.dump(tweets, f, indent=4)
@@ -38,8 +43,13 @@ def check(keyword,no_of_tweets=0):
 
 
 if __name__ == '__main__':
+    with open("variables.ini","r") as json_file:
+        json_object=json.load(json_file)
+    CONSUMER_KEY=json_object['consumer_key']
+    CONSUMER_SECRET=json_object['consumer_secret']
     args = parse_args()
     keyword = args.keyword
-    no_of_tweets=args.no
-    print(keyword)
+    no_of_tweets=int(args.no)
     check(keyword,no_of_tweets)
+    
+
